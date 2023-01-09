ed691e6abacfc6a0594b8fe000b1343d1c77cb19
Soulter <37870767+Soulter@users.noreply.github.com>
2023-01-09 18:55:15 +0800

    fix: 修复了一些已知问题

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
 import cores.qqbot.core as qqBot
 from cores.openai.core import ChatGPT
-import asyncio
-import yaml
-import threading
-# from cores.database.conn import dbConn
 def main():
-    # 读取参数
-    # with open('configs/config.yaml', 'r', encoding='utf-8') as f:
-    #     cfg = yaml.safe_load(f)
-    # chatGPT_configs = cfg['openai']['chatGPTConfigs']
-    # print(chatGPT_configs)
     #实例化ChatGPT
     chatgpt = ChatGPT()
-    # db = dbConn()
     # #执行qqBot
-    # qqBot.initBot(chatgpt, db)
     qqBot.initBot(chatgpt)
 
 if __name__ == "__main__":
-    # qqbot_thread = threading.Thread(target=main)
-    # qqbot_thread.start()
     main()
-
