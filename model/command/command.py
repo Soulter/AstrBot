1db9b9b3410c089078a7a5cfd048b177026debaa
Soulter <37870767+Soulter@users.noreply.github.com>
2023-03-31 20:05:23 +0800

    refactor: 重构部分代码-officialapi

---
+++ model/command/command.py
@@ -0,0 +1,38 @@
+import abc
+
+class Command:
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def check_command(self, message):
+        pass
+
+    def reset(self):
+        return False
+    
+    def set(self):
+        return False
+    
+    def unset(self):
+        return False
+    
+    def key(self):
+        return False
+    
+    def help(self):
+        return False
+    
+    def status(self):
+        return False
+    
+    def token(self):
+        return False
+    
+    def his(self):
+        return False
+    
+    def draw(self):
+        return False
+    
+    
---
+++ model/provider/provider_rev_chatgpt.py
@@ -0,0 +1,34 @@
+from revChatGPT.V1 import Chatbot
+from provider import Provider
+
+class ProviderRevChatGPT(Provider):
+    def __init__(self, config):
+        if 'password' in config:
+            config['password'] = str(config['password'])
+        self.bot = Chatbot(config=config)
+
+    def forget(self) -> bool:
+        self.bot.reset_chat()
+        return True
+
+    def text_chat(self, prompt):
+        resp = ''
+        err_count = 0
+        retry_count = 5
+
+        while err_count < retry_count:
+            try:
+                for data in self.bot.ask(prompt):
+                    resp = data["message"]
+                break
+            except BaseException as e:
+                try:
+                    print("[RevChatGPT] 请求出现了一些问题, 正在重试。次数"+str(err_count))
+                    err_count += 1
+                    if err_count >= retry_count:
+                        raise e
+                except BaseException:
+                    err_count += 1
+        
+        print("[RevChatGPT] "+str(resp))
+        return resp
