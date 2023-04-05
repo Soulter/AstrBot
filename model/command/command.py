f6ef46b8082aab40fd88b9729669e390f592141f
Soulter <37870767+Soulter@users.noreply.github.com>
2023-04-05 10:50:27 +0800

    feat: 支持切换语言模型 #55

--- model/command/command.py
+++ model/command/command.py
@@ -76,15 +76,7 @@ class Command:
         return False
     
     def help(self):
-        # ol_version = 'Unknown'
-        # try:
-        #     res = requests.get("https://soulter.top/channelbot/update.json")
-        #     res_obj = json.loads(res.text)
-        #     ol_version = res_obj['version']
-        # except BaseException:
-        #     pass
         return True, f"[Github项目名: QQChannelChatGPT，有问题请前往提交issue，欢迎Star此项目~]\n\n指令面板：\nstatus 查看机器人key状态\ncount 查看机器人统计信息\nreset 重置会话\nhis 查看历史记录\ntoken 查看会话token数\nhelp 查看帮助\nset 人格指令菜单\nkey 动态添加key"
-    
     
     def status(self):
         return False
