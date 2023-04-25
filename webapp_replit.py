9887cae43c4efc7b438e8d9cdaef6bbc732bab2a
Soulter <37870767+Soulter@users.noreply.github.com>
2023-04-25 20:57:22 +0800

    fix: replit web fix

--- webapp_replit.py
+++ webapp_replit.py
@@ -9,13 +9,15 @@ app = Flask(__name__)
 def main_func():
   content = "<h1>QQChannelChatGPT Web APP</h1>"
 
-  content = "<p>" + "Online @ " + str(datetime.datetime.now()) + "</p>"
-  content = "<p>欢迎Star本项目！！！</p>"
+  content += "<p>" + "Online @ " + str(datetime.datetime.now()) + "</p>"
+  content += "<p>欢迎Star本项目！！！</p>"
   return content
 
 
 def run():
   app.run(host="0.0.0.0", port=8080)
+  
+
 
 
 def keep_alive():
