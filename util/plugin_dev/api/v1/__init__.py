9e443f29250da240669ca6a46a21fc01f492b55b
Soulter <905617992@qq.com>
2024-09-11 16:47:44 +0800

    feat: middleware

---
+++ util/plugin_dev/api/v1/__init__.py
@@ -0,0 +1,7 @@
+from .bot import *
+from .config import *
+from .llm import *
+from .message import *
+from .platform import *
+from .register import *
+from .types import *
--- util/plugin_dev/api/v1/types.py
+++ util/plugin_dev/api/v1/types.py
@@ -3,5 +3,5 @@
 '''
 
 from type.plugin import PluginType
-
+from type.middleware import Middleware
 from nakuru.entities.components import Image, Plain, At, Node, BaseMessageComponent
