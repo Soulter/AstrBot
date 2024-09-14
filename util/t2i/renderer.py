6d037059ef6efbb79094e63275ed53cd0cafd235
Soulter <905617992@qq.com>
2024-09-14 19:59:30 +0800

    feat: custom t2i tmpl

--- util/t2i/renderer.py
+++ util/t2i/renderer.py
@@ -12,7 +12,20 @@ class TextToImageRenderer:
         self.local_strategy = LocalRenderStrategy()
         self.context = RenderContext(self.network_strategy)
 
+    async def render_custom_template(self, tmpl_str: str, tmpl_data: dict, return_url: bool = False):
+        '''使用自定义文转图模板。该方法会通过网络调用 t2i 终结点图文渲染API。
+        @param tmpl_str: HTML Jinja2 模板。
+        @param tmpl_data: jinja2 模板数据。
+
+        @return: 图片 URL 或者文件路径，取决于 return_url 参数。
+
+        @example: 参见 https://astrbot.soulter.top 插件开发部分。
+        '''
+        await self.network_strategy.render_custom_template(**locals())
+
     async def render(self, text: str, use_network: bool = True, return_url: bool = False):
+        '''使用默认文转图模板。
+        '''
         if use_network:
             try:
                 return await self.context.render(text, return_url=return_url)
