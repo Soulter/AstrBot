05ccc1995b06c42c0eca2f20a15881c00c13dd2a
Soulter <905617992@qq.com>
2025-01-18 14:18:03 +0800

    fix: 清除残留的 personalities

--- astrbot/api/all.py
+++ astrbot/api/all.py
@@ -1,7 +1,6 @@
 
 from astrbot.core.config.astrbot_config import AstrBotConfig
 from astrbot import logger
-from astrbot.core.utils.personality import personalities
 from astrbot.core import html_renderer
 from astrbot.core.star.register import register_llm_tool as llm_tool
 
