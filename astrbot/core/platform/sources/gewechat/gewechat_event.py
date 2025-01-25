9b1a28c1cabc4d709e8c7818e3bda05f2e899e88
Soulter <905617992@qq.com>
2025-01-25 16:34:40 +0800

    feat: gewechat 支持语音、图片

--- astrbot/core/platform/sources/gewechat/gewechat_event.py
+++ astrbot/core/platform/sources/gewechat/gewechat_event.py
@@ -1,6 +1,7 @@
 import random
 import asyncio
-from astrbot.core.utils.io import download_image_by_url
+import os
+from astrbot.core.utils.io import save_temp_img, download_image_by_url
 from astrbot.api import logger
 from astrbot.api.event import AstrMessageEvent, MessageChain
 from astrbot.api.platform import AstrBotMessage, PlatformMetadata
@@ -34,5 +35,22 @@ class GewechatPlatformEvent(AstrMessageEvent):
         for comp in message.chain:
             if isinstance(comp, Plain):
                 await self.client.post_text(to_wxid, comp.text)
+            elif isinstance(comp, Image):
+                img_url = comp.file
+                img_path = ""
+                if img_url.startswith("file:///"):
+                    with open(comp.file[8:], "rb") as f:
+                        img_path = save_temp_img(f.read())
+                elif comp.file and comp.file.startswith("http"):
+                    img_path = await download_image_by_url(comp.file)
+                    
+                if not img_path:
+                    logger.error("无法获取到图片路径。")
+                    return
+
+                file_id = os.path.basename(img_path).split(".")[0]
+                img_url = f"{self.client.file_server_url}/{file_id}"
+                logger.debug(f"gewe callback img url: {img_url}")
+                await self.client.post_image(to_wxid, img_url)
         
         await super().send(message)
