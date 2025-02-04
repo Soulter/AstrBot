1d427e2645b8ef362945d3f23e14889a22f360e3
Soulter <905617992@qq.com>
2025-02-05 01:10:53 +0800

    perf: 优化插件页面

--- dashboard/src/components/shared/ExtensionCard.vue
+++ dashboard/src/components/shared/ExtensionCard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 const props = defineProps({
   title: String,
-  link: String
+  link: String,
+  logo: String
 });
 
 const open = (link: string | undefined) => {
@@ -13,6 +14,7 @@ const open = (link: string | undefined) => {
   <v-card variant="outlined" elevation="0" class="withbg">
     <v-card-item style="padding: 10px 14px">
       <div class="d-sm-flex align-center justify-space-between">
+        <img v-if="logo" :src="logo" alt="logo" style="width: 40px; height: 40px; margin-right: 8px;">
         <v-card-title style="font-size: 17px;">{{ props.title }}</v-card-title>
         <v-spacer></v-spacer>
         <v-btn variant="plain" @click="open(props.link)">仓库</v-btn>
