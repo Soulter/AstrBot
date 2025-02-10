9f02dd13ff48c9d21cda0d39da65e259f453aea4
Soulter <905617992@qq.com>
2025-02-10 13:07:09 +0800

    fix: 修复qq在@和回复开启的情况下转发消息异常的问题

--- astrbot/core/platform/sources/aiocqhttp/aiocqhttp_message_event.py
+++ astrbot/core/platform/sources/aiocqhttp/aiocqhttp_message_event.py
@@ -1,7 +1,7 @@
-import os
+import asyncio
 
 from astrbot.api.event import AstrMessageEvent, MessageChain
-from astrbot.api.message_components import Plain, Image, Record, At
+from astrbot.api.message_components import Plain, Image, Record, At, Node, Music, Video
 from aiocqhttp import CQHttp
 from astrbot.core.utils.io import file_to_base64, download_image_by_url
 
@@ -18,7 +18,7 @@ class AiocqhttpMessageEvent(AstrMessageEvent):
             d = segment.toDict()
             if isinstance(segment, Plain):
                 d['type'] = 'text'
-            if isinstance(segment, (Image, Record)):
+            elif isinstance(segment, (Image, Record)):
                 # convert to base64
                 if segment.file and segment.file.startswith("file:///"):
                     bs64_data = file_to_base64(segment.file[8:])
@@ -31,7 +31,7 @@ class AiocqhttpMessageEvent(AstrMessageEvent):
                 d['data'] = {
                     'file': bs64_data,
                 }
-            if isinstance(segment, At):
+            elif isinstance(segment, At):
                 d['data'] = {
                     'qq': str(segment.qq) # 转换为字符串
                 }
@@ -40,7 +40,19 @@ class AiocqhttpMessageEvent(AstrMessageEvent):
 
     async def send(self, message: MessageChain):
         ret = await AiocqhttpMessageEvent._parse_onebot_json(message)
-        if os.environ.get('TEST_MODE', 'off') == 'on':
-            return
-        await self.bot.send(self.message_obj.raw_message, ret)
+        
+        send_one_by_one = False
+        for seg in message.chain:
+            if isinstance(seg, (Node, Music)):
+                # 转发消息不能和普通消息混在一起发送
+                send_one_by_one = True
+                break
+        
+        if send_one_by_one:
+            for seg in message.chain:
+                await self.bot.send(self.message_obj.raw_message, await AiocqhttpMessageEvent._parse_onebot_json(MessageChain([seg])))
+                await asyncio.sleep(0.5)
+        else:
+            await self.bot.send(self.message_obj.raw_message, ret)
+        
         await super().send(message)
