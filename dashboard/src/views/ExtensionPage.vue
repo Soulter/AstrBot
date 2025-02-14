fc6dcfaf21bbf2a944b9c8c04df9cfb5df2abcc1
Soulter <905617992@qq.com>
2025-02-14 18:45:56 +0800

    🐛 fix: cannot search plugin

--- dashboard/src/views/ExtensionPage.vue
+++ dashboard/src/views/ExtensionPage.vue
@@ -109,7 +109,7 @@ import { max } from 'date-fns';
 
           <v-spacer></v-spacer>
 
-          <v-text-field v-model="search" density="compact" label="Search" prepend-inner-icon="mdi-magnify"
+          <v-text-field v-model="marketSearch" density="compact" label="Search" prepend-inner-icon="mdi-magnify"
             variant="solo-filled" flat hide-details single-line></v-text-field>
         </v-card-title>
 
