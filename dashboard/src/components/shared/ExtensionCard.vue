c7f9c7f81617e14464fbd150822ac6f7c4b4e9e6
Soulter <905617992@qq.com>
2025-02-14 18:42:45 +0800

    ✨ feat: webui supports to search plugin via name

--- dashboard/src/components/shared/ExtensionCard.vue
+++ dashboard/src/components/shared/ExtensionCard.vue
@@ -4,6 +4,7 @@ const props = defineProps({
   link: String,
   logo: String,
   has_update: Boolean,
+  activated: Boolean,
 });
 
 const open = (link: string | undefined) => {
@@ -16,8 +17,9 @@ const open = (link: string | undefined) => {
     <v-card-item style="padding: 10px 12px">
       <div class="d-sm-flex align-center justify-space-between">
         <img v-if="logo" :src="logo" alt="logo" style="width: 40px; height: 40px; margin-right: 8px;">
-        <v-card-title style="font-size: 16px;">{{ props.title }}</v-card-title>
+        <v-card-title style="font-size: 15px; max-width: 70%">{{ props.title }}</v-card-title>
         <v-spacer></v-spacer>
+        <v-icon color="success" v-if="!activated">mdi-cancel</v-icon>
         <v-icon color="success" v-if="has_update">mdi-arrow-up-bold</v-icon>
         <v-btn size="small" text="Read" variant="flat" border  @click="open(props.link)">帮助</v-btn>
       </div>
