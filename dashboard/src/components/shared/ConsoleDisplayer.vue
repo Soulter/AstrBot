8908435148c002f23d6207c68fa298bb945f4562
崔永亮 <702625325@qq.com>
2025-02-22 17:10:53 +0800

    perf: 添加控制台关闭自动滚动按钮

--- dashboard/src/components/shared/ConsoleDisplayer.vue
+++ dashboard/src/components/shared/ConsoleDisplayer.vue
@@ -13,6 +13,7 @@ export default {
   name: 'ConsoleDisplayer',
   data() {
     return {
+      autoScroll: true,  // 默认开启自动滚动
       logColorAnsiMap: {
         '\u001b[1;34m': 'color: #0000FF; font-weight: bold;', // bold_blue
         '\u001b[1;36m': 'color: #00FFFF; font-weight: bold;', // bold_cyan
@@ -54,6 +55,9 @@ export default {
     }
   },
   methods: {
+    toggleAutoScroll() {
+      this.autoScroll = !this.autoScroll;
+    },
     printLog(log) {
       // append 一个 span 标签到 term，block 的方式
       let ele = document.getElementById('term')
@@ -70,7 +74,9 @@ export default {
       span.classList.add('fade-in')
       span.innerText = log
       ele.appendChild(span)
-      ele.scrollTop = ele.scrollHeight
+      if (this.autoScroll) {
+        ele.scrollTop = ele.scrollHeight
+      }
     }
   },
 }
