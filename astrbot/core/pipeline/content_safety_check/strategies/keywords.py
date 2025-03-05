e0da5bb943720a0c977a38d537f23a9b49ef9ee5
Soulter <905617992@qq.com>
2025-03-05 19:05:33 +0800

    chore: delete some files for project safety

--- astrbot/core/pipeline/content_safety_check/strategies/keywords.py
+++ astrbot/core/pipeline/content_safety_check/strategies/keywords.py
@@ -1,7 +1,4 @@
 import re
-import os
-import json
-import base64
 from . import ContentSafetyStrategy
 
 
@@ -11,13 +8,13 @@ class KeywordsStrategy(ContentSafetyStrategy):
         if extra_keywords is None:
             extra_keywords = []
         self.keywords.extend(extra_keywords)
-        keywords_path = os.path.join(os.path.dirname(__file__), "unfit_words")
+        # keywords_path = os.path.join(os.path.dirname(__file__), "unfit_words")
         # internal keywords
-        if os.path.exists(keywords_path):
-            with open(keywords_path, "r", encoding="utf-8") as f:
-                self.keywords.extend(
-                    json.loads(base64.b64decode(f.read()).decode("utf-8"))["keywords"]
-                )
+        # if os.path.exists(keywords_path):
+        #     with open(keywords_path, "r", encoding="utf-8") as f:
+        #         self.keywords.extend(
+        #             json.loads(base64.b64decode(f.read()).decode("utf-8"))["keywords"]
+        #         )
 
     def check(self, content: str) -> bool:
         for keyword in self.keywords:
