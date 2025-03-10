9059d1fb17ab1eed839e5fb14c2d319d046d01cf
Soulter <905617992@qq.com>
2025-03-11 00:34:22 +0800

    ✨ feat: 支持在对话隔离情况下可以将群聊加入白名单 #746

--- packages/astrbot/main.py
+++ packages/astrbot/main.py
@@ -262,8 +262,15 @@ class Main(star.Star):
         """获取会话 ID 和 管理员 ID"""
         sid = event.unified_msg_origin
         user_id = str(event.get_sender_id())
-        ret = f"""SID: {sid} 此 ID 可用于设置会话白名单。/wl <SID> 添加白名单, /dwl <SID> 删除白名单。
-UID: {user_id} 此 ID 可用于设置管理员。/op <UID> 授权管理员, /deop <UID> 取消管理员。"""
+        ret = f"""SID: {sid} 此 ID 可用于设置会话白名单。
+/wl <SID> 添加白名单, /dwl <SID> 删除白名单。
+
+UID: {user_id} 此 ID 可用于设置管理员。
+/op <UID> 授权管理员, /deop <UID> 取消管理员。"""
+
+        if self.context.get_config()["platform_settings"]["unique_session"] and event.get_group_id():
+            ret += f"\n\n当前处于独立会话模式, 此群 ID: {event.get_group_id()}, 也可将此 ID 加入白名单来放行整个群聊。"
+
         event.set_result(MessageEventResult().message(ret).use_t2i(False))
 
     @filter.permission_type(filter.PermissionType.ADMIN)
