ef86838f628de8541f809fef70c5b8af2b062792
邹永赫 <1259085392@qq.com>
2025-03-15 12:15:05 +0900

    修改注册函数工具时的打印信息

--- astrbot/core/star/register/star_handler.py
+++ astrbot/core/star/register/star_handler.py
@@ -358,7 +358,7 @@ def register_llm_tool(name: str = None):
                 }
             )
         md = get_handler_or_create(awaitable, EventType.OnCallingFuncToolEvent)
-        llm_tools.add_func(llm_tool_name, args, docstring.description, md.handler)
+        llm_tools.add_func(llm_tool_name, args, docstring.description.strip(), md.handler)
         return awaitable
 
     return decorator
