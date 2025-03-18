f9e0a95c5e196a748d964077a686146743250cb7
Soulter <905617992@qq.com>
2025-03-18 22:15:22 +0800

    chore: 默认地址改回 0.0.0.0

--- astrbot/core/config/default.py
+++ astrbot/core/config/default.py
@@ -85,7 +85,7 @@ DEFAULT_CONFIG = {
         "enable": True,
         "username": "astrbot",
         "password": "77b90590a8945a7d36c963981a307dc9",
-        "host": "127.0.0.1",
+        "host": "0.0.0.0",
         "port": 6185,
     },
     "platform": [],
